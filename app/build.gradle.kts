3d362684d32fd45530dca0b7abc7babf1b239ecb
ArniSol <solomahinaarina223@gmail.com>
2026-03-21 12:52:12 +0300

    день 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,5 +62,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.activity:activity-ktx:1.8.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
 }
---
+++ app/src/main/java/ru/martuanov/menu/adapter/PostsAdapter.kt
@@ -0,0 +1,26 @@
+package ru.martuanov.menu.adapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import ru.martuanov.menu.databinding.CardPostBinding
+import ru.martuanov.menu.dto.Post
+
+class PostsAdapter(
+    private val onLikeClickListener: (Post) -> Unit,
+    private val onShareClickListener: (Post) -> Unit
+) : ListAdapter<Post, PostViewHolder>(PostDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = CardPostBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return PostViewHolder(binding, onLikeClickListener, onShareClickListener)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val post = getItem(position)  // getItem предоставляет ListAdapter
+        holder.bind(post)
+    }
+}
